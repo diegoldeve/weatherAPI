2823baaf8ce8ba931edd448990d17daa15d80c80
Diego <diegoldeve@gmail.com>
2023-01-17 00:29:03 -0600

    Project finished

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const search = document.querySelector('#search');
 const alerta = document.querySelector('.error');
-alerta.style.display = 'none'
-search.addEventListener('click', validation)
+const resultado = document.querySelector('.resultado');
+const spinner = document.querySelector('.spinner');
+
+alerta.style.display = 'none';
+spinner.style.display = 'none';
+search.addEventListener('click', validation);
 
 function validation(e){
     e.preventDefault();
@@ -11,17 +15,49 @@ function validation(e){
         mostrarError();
         return;
     }
-    console.log(`${city} - ${country}`)
+    searchCity(city,country);
 
 }
 
-function searchCity(){
-
+async function searchCity(city, country){
+    try{
+        const id = '51b8eb88e945e2a6ffe283fbcadbec28';
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${id}`;
+        let data = await fetch(url);
+        let res = await data.json();
+        console.log(res);
+        mostrarHTML(res, city,country)
+        if(res.cod>400){
+            mostrarError();
+        }
+    }catch (error){
+        // console.log(error);
+        
+    }
+    // fetch(url)
+    // .then(data=>data.json)
+    // .then(res=>console.log(res))
 }
 
 function mostrarError(){
-    alerta.style.display = 'block';
+    alerta.style.display = 'flex';
     setTimeout(()=>{
         alerta.style.display = 'none';
-    },1500)
+    },1000)
+}
+
+function mostrarHTML({main}, city, country){
+    resultado.textContent = '';
+    spinner.style.display = 'block';
+    setTimeout(()=>{
+        spinner.style.display = 'none';
+        const {temp} = main;
+        const tempUpdate = temp-273.12;
+        const tempActual = tempUpdate.toFixed(2)
+        resultado.innerHTML = `
+            <h2 class='city'>${city} - ${country}</h2>
+            <p class='temp'>Temperatura actual: <b>${tempActual}</b>°C</p>
+            
+        `
+    },1000)
 }
